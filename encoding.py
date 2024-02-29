340ece8db434bd23272d559c854f3b184a677668
ethanvr5 <ethanvanruyskensvelde@gmail.com>
2024-02-29 17:12:46 +0100

    ok

--- encoding.py
+++ encoding.py
@@ -8,10 +8,6 @@ from pixel import Pixel
 from image import Image
 
 
-
-"""regarder problème d'encodage de l'image sur les lignes !!!"""
-
-
 class Encoder:
     def __init__(self, img, version=1, **kwargs):
         """
